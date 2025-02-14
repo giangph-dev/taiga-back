fe9596439adec6ba32546c6def1fcc8c9fdebc82
Vo Minh Tri <vo.minh-tri1@hdsaison.com.vn>
2025-02-14 14:53:46 +0700

    update text

--- taiga/external_apps/management/commands/create_taiga_tribe_app.py
+++ taiga/external_apps/management/commands/create_taiga_tribe_app.py
@@ -11,13 +11,13 @@ from taiga.external_apps.models import Application
 
 class Command(BaseCommand):
     args = ''
-    help = 'Create Taiga Tribe external app information'
+    help = 'Create Project Management Tribe external app information'
 
     def handle(self, *args, **options):
         Application.objects.get_or_create(
             id="8836b290-9f45-11e5-958e-52540016141a",
-            name="Taiga Tribe",
-            icon_url="https://tribe.taiga.io/static/common/graphics/logo/reindeer-color.png",
+            name="Project Management Tribe",
+            icon_url="https://www.hdsaison.com.vn/vnt_upload/weblink/Logo_website_01.png",
             web="https://tribe.taiga.io",
             description="A task-based employment marketplace for software development.",
             next_url="https://tribe.taiga.io/taiga-integration",
